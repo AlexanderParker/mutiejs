183fc097c9d9624409ad7168850924ba4470e841
Alexander Parker <alexofparker@gmail.com>
2015-03-08 19:07:10 +0800

    Update demo

--- src/client/mutie.js
+++ src/client/mutie.js
@@ -1,5 +1,17 @@
 var mutiejs = mutiejs || {};
 
-mutiejs.bootstrap = function() {
-	console.log('hello world');
+// Basic session key functionality.
+mutiejs.getSessionKey = function() {
+    var sessionKey = document.cookie.replace(/(?:(?:^|.*;\s*)mutiesession\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    if (!sessionKey) {
+        // Create a basic random ID.  Will need to look out for collisions later on.
+        document.cookie = "mutiesession=" + Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2);
+    }
+    return document.cookie.replace(/(?:(?:^|.*;\s*)mutiesession\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+}
+
+// Function to start the fun:
+mutiejs.bootstrap = function() {    
+    var sessionKey = this.getSessionKey();
+    console.log(sessionKey);
 }
