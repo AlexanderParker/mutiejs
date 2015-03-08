e86cc1ba74a2055e339ff675d13a51af8df60527
Alexander Parker <alexofparker@gmail.com>
2015-03-08 17:54:34 +0800

    Add MutableElement and managers

--- src/server.js
+++ src/server.js
@@ -2,42 +2,100 @@
 var server = function() {
 
 	// Goal object:
-	function Goal(config) {
+	function Goal(config) {		
+		this.type = config.type;
 		this.data = config.data;
-		this.prototype.process = function() {
-			// Update all associated mutation scores
+	}
 
+	// Manage configured goals:
+	function GoalManager(config) {
+		if (config.goals.constructor === Array) {
+			this.goals = config.goals;
+		}
+		else {
+			this.goals = [];
+		}
+		this.prototype.push = function(goal) {
+			if (goal.constructor === Goal) {
+				this.goals.push(goal);
+			}
+			else {
+				throw _exception(
+					"Attempted to push non-Goal object to GoalManager goals",
+					goal
+				);
+			}
 		}
 	}
 
-	function Element(config) {
+	// ElementManager object:
+	function ElementManager(config) {
+		if (config.elements.constructor === Array) {
+			this.elements = config.elements;
+		}
+		else {
+			this.elements = [];
+		}
+		this.prototype.push = function(element) {
+			if (element.constructor === MutableElement) {
+				this.elements.push(element);
+			}
+			else {
+				throw _exception(
+					"Attempted to push non-MutableElement object to ElementManager elements",
+					element
+				);
+			}
+		}
+	}
+
+	// MutableElement object:
+	function MutableElement(config) {
+		this.elementId = config.elementId;
+	}
+
+	// ElementMutations object:
+	function ElementMutations(config) {
+		// MutableElement reference is required
+		if (config.element.constructor === MutableElement) {
+			this.element = config.element;
+		}
+		else {
+			throw _exception("ElementMutations requires a MutableElement in constructor.");
+		}
+		// Mutations list may be optional:
 		if (config.mutations.constructor === Array) {
 			this.mutations = config.mutations;
 		}
 		else {
 			this.mutations = [];
-		}	
+		}
+		// Retrieve mutations associated with the element:
 		this.prototype.getMutations = function() {
 			return this.mutations;
 		}
+		// Retrieve the element's primary mutation:
 		this.prototype.getPrimaryMutation = function() {
 			// Grab the primary mutation for the element;
 			return this.mutations.find(function isPrimary(element, index, array) {
 				return element.isPrimary;
 			});
 		}
+		// Retrieve the alternative mutations:
 		this.prototype.getAlternateMutations = function() {
 			// Grab the alternate mutations for the element;
 			return this.mutations.filter(function isPrimary(element, index, array) {
 				return !element.isPrimary;
 			});
 		}
+		// Fetch a random mutation for the element:
 		this.prototype.getRandomMutation = function() {
 			// Retrieve a random mutation for the element;
 			return this.mutations[
 				Math.floor(Math.random()*this.mutations.length)
 			];
 		}
+		// Add a new mutation to the element:
 		this.prototype.addMutation = function(mutation) {
 			if (mutation.constructor === Mutation) {
 				this.mutations.push(mutation);
@@ -73,24 +131,24 @@ var server = function() {
 	}
 
 	// A place to store goal notifications.
-	var goalProcessingQueue = function(items) {
+	var goalResultProcessingQueue = function(items) {
 		if (items.constructor === Array) {
 			this.items = items;
 		}
 		else {
 			this.items = [];
-		}		
+		}
 		this.prototype.shift = function() {
 			return this.items.shift();
 		}
-		this.prototype.push = function(goal) {
+		this.prototype.push = function(goalResult) {
 			// You can only push valid Goals to the queue:
-			if (goal.constructor === Goal) {
-				return this.items.push(goal);
+			if (goal.constructor === GoalResult) {
+				return this.items.push(goalResult);
 			}
-			else {				
+			else {
 				throw _exception(
-					"Attempted to push non-Goal object to goalProcessingQueue",
+					"Attempted to push non-GoalResult object to goalResultProcessingQueue",
 					goal
 				);
 			}
@@ -124,16 +182,17 @@ var server = function() {
 	// Push goal success to the queue:
 	function _pushGoalSuccess(data) {
 		// Insert data into a Goal instance in the queue.
-		goalProcessingQueue.push(new Goal({
-			data: data
+		goalProcessingQueue.push(new GoalResult({
+			goal: new Goal(data.goal)
 		}));
 		return true;			
 	}
 
 	// Goal success processing callback:
 	function _processGoalQueue() {
-		if (goalProcessingQueue.length > 0) {
-			goalProcessingQueue.shift.process();
+		// Process the most recent goal result.
+		if (goalResultProcessingQueue.length > 0) {
+			goalResultProcessingQueue.shift.process();
 		}
 	}
 
